6d25a6a22bb178e00af7a434af3b774198b94840
Victor Yves Crispim <rotcivyves@hotmail.com>
2024-11-21 15:43:04 -0300

    print book report

--- main.py
+++ main.py
@@ -13,7 +13,30 @@ def count_characters(book):
                 chars[char] = 1
     return chars
 
+
+def sort_on(dict):
+    return dict["count"]
+
+def generate_book_report(book):
+    report_list = []
+    total_words = count_words(book)
+    report_list.append(f"{total_words} words found in the document")
+    
+    character_count = count_characters(book)
+    char_list = []
+    for char, count in character_count.items():
+        char_list.append({"char": char, "count": count})
+    char_list.sort(reverse=True, key=sort_on)
+
+    for dict in char_list:
+        report_list.append(f"The '{dict["char"]}' character was found {dict["count"]} times")
+        
+    return "\n".join(report_list)
+
+
+
 with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+    book = f.read()
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(generate_book_report(book))
+    print("--- End report ---")
