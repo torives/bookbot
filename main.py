a2b469fe590a2509008c375d0aea81e7a870fb81
Victor Yves Crispim <rotcivyves@hotmail.com>
2024-11-21 14:52:50 -0300

    count words

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def count_words(book):
+    words = book.split()
+    return len(words)
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+    print(count_words(file_contents))
